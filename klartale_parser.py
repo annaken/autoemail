c2db82f06819d096681abfb26518b4690d43af48
annaken <annaken@redpill-linpro.com>
2014-11-04 12:53:24 +0100

    Initial commit

---
+++ klartale_parser.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+from optparse import OptionParser
+import ConfigParser
+import urllib
+#from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+enc = 'utf-8'
+
+# Get outbout email settings from config
+emailconfig = ConfigParser.RawConfigParser()
+emailconfig.read('email.conf')
+
+smtpserver   = emailconfig.get('Email', 'smtpserver')
+from_addr    = emailconfig.get('Email', 'from_addr')
+to_addr_list = emailconfig.get('Email', 'to_addr_list').split(',')
+login        = emailconfig.get('Email', 'login')
+password     = emailconfig.get('Email', 'password')
+
+# Get the content from the website
+sock = urllib.urlopen("http://www.klartale.no/")
+webpage = sock.read()
+sock.close
+
+# Parse the HTML
+phtml = BeautifulSoup(webpage)
+
+# Find the main content
+maincontent = phtml.body.find('div', attrs={'class':'mainContent'})
+
+# Get the main article's title
+article_title = maincontent.h2.a.span.string
+enc_article_title = unicode(article_title).encode(enc)
+subject = "Klar Tale " + enc_article_title
+
+# and short content
+article_text = maincontent.find('div', attrs={'class':'subtext'}).contents[5]
+enc_article_text = unicode(article_text).encode(enc)
+text = enc_article_title + "\n\n" + enc_article_text
+
+# now go to the other page and get the full content
+articleurl = maincontent.h2.a['href']
+
+# Get the content from that other page
+sock = urllib.urlopen(articleurl)
+articlepage = sock.read()
+sock.close
+
+# Parse the HTML
+parthtml = BeautifulSoup(articlepage)
+
+# Get the article full text
+article_content = parthtml.body.find('div', attrs={'class':'articleText'})
+plist = article_content.findAll('p')
+pstring = ''.join(unicode(p).encode(enc) for p in plist)
+
+html = """\
+<html>
+  <head></head>
+  <body>
+		<p>
+	  	<i><b>""" + enc_article_text + """</b></i><br/><br/>""" + pstring + """<br/><br/>
+  		<a href="http://www.klartale.no">Trykk her for hele siden</a>
+   	</p>
+  </body>
+</html>
+"""
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = subject
+msg['From'] = from_addr
+msg['To'] = ','.join(to_addr_list)
+
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+msg.attach(part1)
+msg.attach(part2)
+
+server = smtplib.SMTP(smtpserver)
+server.starttls()
+server.login(login,password)
+problems = server.sendmail(from_addr, to_addr_list, msg.as_string())
+server.quit()
---
+++ ukc_parser.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+from optparse import OptionParser
+import ConfigParser
+import urllib
+from BeautifulSoup import BeautifulSoup
+#from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+import re
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+enc = 'utf-8'
+
+# Get outbout email settings from config
+emailconfig = ConfigParser.RawConfigParser()
+emailconfig.read('email.conf')
+
+smtpserver   = emailconfig.get('Email', 'smtpserver')
+from_addr    = emailconfig.get('Email', 'from_addr')
+to_addr_list = emailconfig.get('Email', 'to_addr_list').split(',')
+login        = emailconfig.get('Email', 'login')
+password     = emailconfig.get('Email', 'password')
+
+# Get the content from the website
+#sock = urllib.urlopen("http://www.ukclimbing.com/forums/")
+sock = urllib.urlopen("http://www.ukclimbing.com/forums/i.php?f=2")
+webpage = sock.read()
+sock.close
+
+# Parse the HTML
+phtml = BeautifulSoup(webpage)
+
+# Find the main content
+maincontent = phtml.body.find('table', attrs={'class':'small lst'})
+
+# Find all the topics
+searchresults = ""
+
+# Oslo
+topic1 = maincontent.findAll("a", text=re.compile(".*oslo.*", re.IGNORECASE))
+topic2 = maincontent.findAll("a", text=re.compile(".*norway.*", re.IGNORECASE))
+
+searchresults = topic1 + topic2
+
+text = searchresults
+
+html = """\
+<html>
+  <head></head>
+  <body>
+		<p>This is an alert that one or more of your search terms 'Oslo' or 'Norway' is currently being actively discussed on UKC.<br/><br/>
+		Search results:<br/>""" + searchresults + """<br/><br/>
+  		<a href="http://www.ukclimbing.com/forums/">Click here to go to the ukc forum</a>
+   	</p>
+  </body>
+</html>
+"""
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "UKC forum alert"
+msg['From'] = from_addr
+msg['To'] = ','.join(to_addr_list)
+
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+msg.attach(part1)
+msg.attach(part2)
+
+server = smtplib.SMTP(smtpserver)
+server.starttls()
+server.login(login,password)
+problems = server.sendmail(from_addr, to_addr_list, msg.as_string())
+server.quit()
