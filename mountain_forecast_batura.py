b9d6360db50b47b5fc56afa17e12ca361e53347b
Anna Kennedy <annaelizabethkennedy@gmail.com>
2015-06-23 17:32:06 +0000

    works

--- mountain_forecast_batura.py
+++ mountain_forecast_batura.py
@@ -35,51 +35,101 @@ sock.close
 # Parse the HTML
 phtml = BeautifulSoup(webpage)
 
-# Find the first time Gilgit is mentioned (the most recent result)
-#g_head = phtml.body.find(text=re.compile(".*Gilgit.*"))
-# Fint the next sibling to get the description
-#g_desc = g_head.find_next("span").find("span").contents[0]
-
-#text = g_desc
-
+# Get everything
 maincontent = phtml.body.find('div', attrs={'id':'forecast-cont'})
 
+# Get the table where we name the days
 day_tr = maincontent.find('tr', attrs={'class':'lar hea '} )
-#day_list = day_tr.findAll(['td','b'])
-day_list = day_tr.findAll('b')
 
-for d in day_list:
-	print d.contents[0][0]
+daylist = []
 
-exit()
+# What does our column layout look like?
+col_list = day_tr.findAll('td', {"colspan":True})
+for col in col_list:
+
+	# How many columns does the day span?
+	numcols = int(col['colspan'])
+
+	# If it's not three, it's a special case html
+	if len(col.contents)!=3:
+		day = col.contents[0].strip()[0:2].upper()
+		#day = col.contents[0].strip()[0]
+		
+	else:
+		day = col.find('b').contents[0][0:2].upper()
+		#day = col.find('b').contents[0][0]
+
+	# Fill the list with the a day per column	
+	for i in range (0, numcols):
+#		print "numcols", i
+		daylist.append(day)
+
+#print daylist
+
+weatherlist = []
+
+# Weather description
+witems = maincontent.findAll('div', attrs={'class':'weathercell'})
+
+for w in witems:
+	# Get the description
+	wdesc = w.find('img')['alt']
+	
+	# Split the desc into words
+	wdesc_parts = wdesc.split()
+
+	# If it's only one word
+	if len(wdesc_parts)==1:
+		wsdesc = wdesc_parts[0][0] + wdesc_parts[0][-1]
+
+	# If it's two words
+	else:
+		wsdesc = wdesc_parts[0][0] + wdesc_parts[1][0]
+	
+	weatherlist.append(wsdesc)
+
+#print weatherlist
+
+snowlist = []
+
+# Get snow
+snowitems = maincontent.findAll('span', attrs={'class':'snow'})
+
+for s in snowitems:
+	snowlist.append(s.contents[0])
+
+#print snowlist
+
+weathermsg = ''
+
+for i in range(0, 17):
+	
+#	print daylist[i]
+#	print weatherlist[i]
+#	print snowlist[i]
+
+	weathermsg+=daylist[i]
+	weathermsg+=weatherlist[i]
+	weathermsg+=snowlist[i]
+	weathermsg+=' '
 
 
-# Get the main article's title
-article_title = maincontent.h2.a.span.string
-enc_article_title = unicode(article_title).encode(enc)
-subject = "Klar Tale " + enc_article_title
-
-# and short content
-article_text = maincontent.find('div', attrs={'class':'subtext'}).contents[5]
-enc_article_text = unicode(article_text).encode(enc)
-text = enc_article_title + "\n\n" + enc_article_text
+text = weathermsg
 
 
 html = """\
 <html>
   <head></head>
   <body>
-		<p>This is the weather forecast from the Pakistani Metrological Department:<br/>
-		""" + g_head + """ """ + g_desc  + """<br/><br/>
-  		<a href="http://nwfc.pmd.gov.pk/24-hour-weather-outlook/index.php">Click here to go to the website</a>
+		<p>This is the weather forecast from Mountain Forecast for Batura 5000m:<br/>
+		""" + weathermsg  + """<br/><br/>
    	</p>
   </body>
 </html>
 """
 
 msg = MIMEMultipart('alternative')
-#msg['Subject'] = "Gilgit weather forecast"
-msg['Subject'] = g_desc
+msg['Subject'] = "Batura 5000 weather forecast"
 msg['From'] = from_addr
 msg['To'] = ','.join(to_addr_list)
 
